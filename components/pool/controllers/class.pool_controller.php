f41309c4627e9d28d9b69789913e7d1612b8ba4a
Greg Froese <greg@shuddertech.ca>
2010-09-04 01:16:54 -0500

    need components in there

---
+++ components/pool/controllers/class.pool_controller.php
@@ -0,0 +1,17 @@
+<?php // -*- mode:php; tab-width:4; indent-tabs-mode:t; c-basic-offset:4; -*-
+
+/**
+* @author Greg Froese
+*
+*/
+
+class PoolController extends \silk\action\Controller {
+
+function index( $params = null ) {
+	echo "Hello World";
+}
+
+function dashboard($params) {
+}
+
+}
