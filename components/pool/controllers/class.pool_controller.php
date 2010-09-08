4094d6f0d89d821871c72089f9b7ae9e97de5151
Greg Froese <greg@shuddertech.ca>
2010-09-08 01:13:09 -0500

    just proving i can login

--- components/pool/controllers/class.pool_controller.php
+++ components/pool/controllers/class.pool_controller.php
@@ -10,6 +10,16 @@ class PoolController extends \silk\action\Controller {
 function index( $params = null ) {
 	$users = \silk\auth\User::find_all();
 	$this->set('users', $users);
+	foreach( $users as $user ) {
+		$user->set_password( date( "Y-m-d H:i:s" ) );
+		$user->email = date( "Y-m-d H:i:s" );
+		$result = $user->save();
+		var_dump( "save result $result" );
+//		var_dump( $user );
+		$us = new \silk\auth\UserSession( array( "username"=>$user->username, "password"=>"a" ));
+		$result = $us->login();
+		var_dump( "result", $result ); die;
+	}
 }
 
 function dashboard($params) {
